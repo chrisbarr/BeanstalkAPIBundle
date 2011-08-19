722bb4854e8d0b3ad6e954a7d3504f9b9157af20
Chris Barr <chris.barr@ntlworld.com>
2011-08-19 13:05:43 +0100

    Overload __construct() to pass parameters to parent constructor

--- Services/BeanstalkAPI.php
+++ Services/BeanstalkAPI.php
@@ -6,5 +6,8 @@ use Beanstalk\lib\BeanstalkAPI as BeanstalkAPIClass;
 
 class BeanstalkAPI extends BeanstalkAPIClass
 {
-	//
+	public function __construct($account, $username, $password)
+	{
+		parent::__construct($account, $username, $password);
+	}
 }
