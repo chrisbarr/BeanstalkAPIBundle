41cbf1773c15d7c0d335c983fd352f77936e4449
Chris Barr <chris.barr@ntlworld.com>
2011-08-20 23:49:09 +0100

    Added licensing

--- BeanstalkAPIBundle.php
+++ BeanstalkAPIBundle.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Beanstalkapp\BeanstalkAPIBundle
+ *
+ * (c) Chris Barr <chris.barr@ntlworld.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Beanstalkapp\BeanstalkAPIBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
